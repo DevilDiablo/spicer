960d4a7dcb26ea3dd10f883a8fc32f649aebf36a
DevilDiablo <46159189+DevilDiablo@users.noreply.github.com>
2021-07-19 13:31:25 +0530

    abc

--- spicer2/settings.py
+++ spicer2/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static2/'
 STATICFILES_DIRS = ['static2']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
